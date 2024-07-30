d53407fd03d6662257e9837fdadd05734b2791b1
chemirik Mohamed <chemirikmohamed@MacBook-Pro-de-chemirik.local>
2024-07-30 20:51:53 +0200

    Ajout HTML, CSS, JS, mise en place de la homepage du projet O’coffee, mise en place du dossier app, plus public, views, partials et du fichier app.js. Mise en place du path express, fichier statique du fichier header.ejs et footer.ejs.

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Serveur en écoute sur le port ${port}`);
+});
